3120f67287f6d3f1b54b1a45d380d6a4895e909b
4goUnited <4goUnited@gmail.com>
2022-05-23 23:19:32 +0500

    First solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+func ReverseString(input string) string {
+	rn := []rune(input)
+	for i, j := 0, len(rn)-1; i < len(rn)/2; i, j = i+1, j-1 {
+		rn[i], rn[j] = rn[j], rn[i]
+	}
+	return string(rn)
 }
